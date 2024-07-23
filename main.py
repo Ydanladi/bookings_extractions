0999665eafe73df6f0475eb1f05bdc894c10ae8b
Mr yusuf <ydanladi2021@gmail.com>
2024-07-23 21:25:02 +0100

    new2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import time
-import random
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
+# import time
+# import random
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 
 import streamlit as st
